60ee2e75c1ccd47328198405a6c64b3a88acc633
Bekzod <mirzaahmedov.dev@gmail.com>
2023-12-30 14:22:47 +0500

    added variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { useRef, useState, useLayoutEffect } from "react";
+import { useRef, useState, useEffect, useLayoutEffect } from "react";
+import { Plus, Equals, X } from "@phosphor-icons/react";
 
 type Equation = {
   problem: string;
@@ -20,14 +21,20 @@ function evalEquation(equation: string): string {
     return "?";
   }
 }
+function evalVariables(equation: string, variables: Variable[]): string {
+  variables.forEach((v) => {
+    equation = equation.replace(`$${v.name}`, v.value);
+  });
+  return equation;
+}
 
 function App() {
   const inputRefs = useRef<(HTMLInputElement | null)[]>([]);
   const [activeEquation, setActiveEquation] = useState<number>(0);
+  const [variables, setVariables] = useState<Variable[]>([]);
   const [equations, setEquations] = useState<Equation[]>([
     { problem: "", result: "" },
   ]);
-  const [variables, setVariables] = useState<Variable[]>([]);
 
   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === "Enter") {
@@ -40,23 +47,46 @@ function App() {
       setEquations((state) =>
         state.map((v, j) =>
           j === i
-            ? { problem: e.target.value, result: evalEquation(e.target.value) }
+            ? {
+                problem: e.target.value,
+                result: evalEquation(evalVariables(e.target.value, variables)),
+              }
             : v,
         ),
       );
     };
   };
+  const handleChangeVariable = (i: number, key: keyof Variable) => {
+    return (e: React.ChangeEvent<HTMLInputElement>) => {
+      setVariables((state) =>
+        state.map((v, j) => (j === i ? { ...v, [key]: e.target.value } : v)),
+      );
+    };
+  };
 
+  useEffect(() => {
+    setEquations((state) =>
+      state.map((e) => ({
+        ...e,
+        result: evalEquation(evalVariables(e.problem, variables)),
+      })),
+    );
+  }, [variables]);
   useLayoutEffect(() => {
     inputRefs.current[activeEquation]?.focus();
-  });
+  }, [activeEquation]);
 
   return (
-    <div>
-      <div>
+    <div className="flex h-full">
+      <div className="flex-1">
         {equations.map(({ problem, result }, i) => (
-          <div key={i} className="border-b border-slate-200 text-slate-900 ">
+          <div
+            key={i}
+            className="flex items-center border-b border-slate-200 text-slate-900 "
+          >
+            <span className="px-4 text-slate-500">{i + 1}.</span>
             <input
+              type="text"
               ref={(ref) => {
                 if (i >= inputRefs.current.length) {
                   inputRefs.current.push(ref);
@@ -66,15 +96,53 @@ function App() {
               }}
               autoFocus={i === activeEquation}
               onFocus={() => setActiveEquation(i)}
-              type="text"
               value={problem}
               onKeyDown={handleKeyDown}
               onChange={handleChangeEquation(i)}
-              className="appearance-none border-none bg-none outline-none p-4 text-2xl tracking-widest"
+              className="appearance-none border-none bg-none outline-none italic p-4 text-md tracking-widest"
             />
-            <span className="text-slate-500">= {result}</span>
+            <span className="flex items-center gap-2 text-slate-500 italic">
+              <Equals /> {result}
+            </span>
           </div>
         ))}
+      </div>
+      <div className="height-full w-full max-w-lg p-4 border-l border-slate-200">
+        {variables.map((v, i) => (
+          <div className="flex items-center gap-2 py-2">
+            <input
+              type="text"
+              value={v.name}
+              onChange={handleChangeVariable(i, "name")}
+              className="min-w-0 flex-1 p-2 bg-slate-100 text-slate-600 italic outline-none rounded"
+            />
+            <span className="text-slate-500">
+              <Equals />
+            </span>
+            <input
+              type="text"
+              value={v.value}
+              onChange={handleChangeVariable(i, "value")}
+              className="min-w-0 flex-1 p-2 bg-slate-100 text-slate-600 italic outline-none rounded"
+            />
+            <button
+              className="p-1 text-slate-500 rounded-full border border-slate-500"
+              onClick={() =>
+                setVariables((state) => state.filter((_, j) => j !== i))
+              }
+            >
+              <X />
+            </button>
+          </div>
+        ))}
+        <button
+          className="flex items-center gap-2 py-3 px-5 mt-4 bg-blue-500 text-white rounded font-medium"
+          onClick={() =>
+            setVariables((state) => [...state, { name: "", value: "" }])
+          }
+        >
+          <Plus /> New Variable
+        </button>
       </div>
     </div>
   );
